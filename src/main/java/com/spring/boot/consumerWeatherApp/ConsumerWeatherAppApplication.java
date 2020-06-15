054994f143cc5c1db0a154dd58a9e0ea30bc013c
basantpatidar <basantpatidar33@gmail.com>
2020-06-15 16:20:01 -0400

    first commit

---
+++ src/main/java/com/spring/boot/consumerWeatherApp/ConsumerWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.consumerWeatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerWeatherAppApplication.class, args);
+	}
+
+}
