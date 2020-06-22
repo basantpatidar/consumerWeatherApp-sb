e3e5bd401ee47853306f11bd11a8f03d24ff6a98
basantpatidar <basantpatidar33@gmail.com>
2020-06-22 16:38:39 -0400

    controller

---
+++ src/main/java/com/spring/boot/consumer/controller/WeatherAlertController.java
@@ -0,0 +1,32 @@
+package com.spring.boot.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.boot.consumer.model.WeatherAlert;
+import com.spring.boot.consumer.service.AlertService;
+
+
+@RestController
+public class WeatherAlertController {
+	
+	@Autowired
+	private AlertService alertService;
+
+	@PostMapping("/addReading")
+	public boolean AddReading(@RequestBody WeatherAlert weatherAlert) {
+		alertService.addAlerts(weatherAlert);
+//		System.out.println("Running");
+		return true;
+	}
+	
+	
+	@GetMapping(value = "/weatheralert")
+//	@RequestMapping(value="test", method=RequestMethod.GET)
+	public String testEndPoint() {
+		return "TEST";
+	}
+}
