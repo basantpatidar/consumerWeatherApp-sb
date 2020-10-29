e0e2dac6370ebfc3a5dbde9c3b84e9c0be132ccc
basantpatidar <basantpatidar33@gmail.com>
2020-10-28 21:19:48 -0400

    sql update

--- src/main/java/com/spring/boot/consumer/repository/WeatherRepository.java
+++ src/main/java/com/spring/boot/consumer/repository/WeatherRepository.java
@@ -1,11 +1,16 @@
 package com.spring.boot.consumer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.spring.boot.consumer.model.Weather;
 
 @Repository
 public interface WeatherRepository extends JpaRepository<Weather, String>{
+	
+	@Query(value="UPDATE weather_reading w SET temperature = 10 WHERE timestamp = '2020-06-16 08:23:59.500000'", nativeQuery=true)
+    public void updateTemp(@Param("city") String username, @Param("temp") double temp);
 
 }
