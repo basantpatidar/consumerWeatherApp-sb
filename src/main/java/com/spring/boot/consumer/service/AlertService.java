2ddb81d631912357e0dd97cf29f4ffb8a7c22689
basantpatidar <basantpatidar33@gmail.com>
2020-11-22 23:24:55 -0500

    update

--- src/main/java/com/spring/boot/consumer/service/AlertService.java
+++ src/main/java/com/spring/boot/consumer/service/AlertService.java
@@ -7,7 +7,7 @@ import com.spring.boot.consumer.model.WeatherAlert;
 
 public interface AlertService {
 	
-	public Boolean addAlerts(List<Weather> weather);
+	public Boolean addAlerts(WeatherAlert weatherAlert);
 	public List<WeatherAlert> getAllAlerts();
 	public Boolean addAlerts(Weather weather);
 }
